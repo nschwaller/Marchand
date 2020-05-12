8a9b10afb779701cdbd88d91e436853f8126757a
nschwaller <55143331+nschwaller@users.noreply.github.com>
2020-05-12 12:04:06 +0200

    Var_Dump
    Var_dump mis en commentaire

--- marchand/panier.php
+++ marchand/panier.php
@@ -39,7 +39,7 @@ require_once('classe.php');
 					$prix="SELECT prixCD FROM CD WHERE ";
 				}
 				
-				var_dump($panier);
+				//var_dump($panier);
 				//echo "<br/> <br/> <br/>";
 					for($i=0;$i<sizeof($panier);$i++)
 					{
